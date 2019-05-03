5e05e82fdd83c938e5933cde9ef1ac2821d0ce8c
Theresa Cyrus <cyrustheresa@gmail.com>
2019-05-03 02:18:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,15 @@ function  nowServing(numberInline){
   }
 }
 function currentLine(numberInline){
-  var stringToReturn = "The line is currently:";
-  for(var i; i < numberInline.length; i++){
-    stringToReturn += (i+1) + ". " + numberInline[i];
-    if(i == numberInline.length - 1)
+  var stringToReturn = "";
+  var stringBuilt =  "The line is currently:";
+  if(numberInline.length == 0){
+    stringToReturn = 
   }
+  for(var i; i < numberInline.length; i++){
+    stringBuilt += (i+1) + ". " + numberInline[i]+",";
+  }
+  
+  return stringToReturn
   
 }
