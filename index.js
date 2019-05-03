02589e2991d080b260dead6d6650005c45d38110
Theresa Cyrus <cyrustheresa@gmail.com>
2019-05-03 02:20:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function currentLine(numberInline){
     for(var i; i < numberInline.length; i++){
       stringBuilt += (i+1) + ". " + numberInline[i]+",";
     }
+    stringToReturn = stringBuilt
   
   }
   return stringToReturn
