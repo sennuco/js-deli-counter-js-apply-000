c7f1260097b132a2b2822eb579a5f1a0d294e1fc
Theresa Cyrus <cyrustheresa@gmail.com>
2019-05-03 01:40:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(numberInline,customerName){
   numberInline.push(customerName)
-  return "Welcome, " +  customerName + ". " + " You are number " + numberInline.length + " in line."
+  return "Welcome, " +  customerName + ". " + "You are number " + numberInline.length + " in line."
 }
