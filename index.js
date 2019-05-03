71c5591fd189aa33eedb931c10f266c5fa7f4ad9
Theresa Cyrus <cyrustheresa@gmail.com>
2019-05-03 01:26:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function takeANumber(namesLine, name){
-  katzDeli.push(name);
-  return "Welcome, "+ name +". You are number "+ (namesLine.length+1)+" in line."
+function takeANumber(katzDeli, name){
+  //katzDeli.push(name);
+  return "Welcome, "+ name +". You are number "+ (katzDeli.length+1)+" in line."
 }
